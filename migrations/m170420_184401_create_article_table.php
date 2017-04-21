72e64a90af739241152fbd84d771be86472ecf03
kharinserhiy <kharinserhiy@gmail.com>
2017-04-21 13:14:42 +0300

    before vidos 8

---
+++ migrations/m170420_184401_create_article_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `article`.
+ */
+class m170420_184401_create_article_table extends Migration
+{
+    public function up()
+    {
+        $this->createTable('article', [
+            'id' => $this->primaryKey(),
+            'title'=>$this->string(),
+            'description'=>$this->text(),
+            'content'=>$this->text(),
+            'date'=>$this->date(),
+            'image'=>$this->string(),
+            'viewed'=>$this->integer(),
+            'user_id'=>$this->integer(),
+            'status'=>$this->integer(),
+            'category_id'=>$this->integer(),
+        ]);
+    }
+    /**
+     * @inheritdoc
+     */
+    public function down()
+    {
+        $this->dropTable('article');
+    }
+}
